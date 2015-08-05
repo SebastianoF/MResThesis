43e0a4e7e4d0869e154e3ad792e38058d72b93a2
SebastianoF <sebastiano.ferraris@gmail.com>
2015-08-05 19:11:21 +0100

    Corrections ch 1 and 5. added counter

---
+++ words_counter.py
@@ -0,0 +1,32 @@
+import os
+import subprocess
+
+
+'''
+    Counts the words in the manually selected latex files in the list list_file
+    
+    Commands to obtain the number of words in a .tex file:
+    'detex my_file.tex | wc -w'
+    where my_file.tex is one of the string in the list list_files
+'''
+
+root_project = os.getcwd()
+
+list_files = ["chapters/Ch_introduction.tex",
+              "chapters/Ch_tools",
+              "chapters/Ch_spatial_transformations",
+              "chapters/Ch_log_computations",
+              "chapters/Ch_results.tex"]
+
+pagg = [0]*len(list_files)
+
+for i in range(len(pagg)):
+    proc = subprocess.Popen(["detex " + list_files[i] + " | wc -w"], stdout=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+    pagg[i] = int(out)
+    print "Number of words in file: " + list_files[i] + " is: " + str(pagg[i])
+
+print "\n Summary:"
+print pagg
+print "\n Total: " + str(sum(pagg)) + " \n"
+print "Thanks for using words count, by Opossum Software Society. \n"
